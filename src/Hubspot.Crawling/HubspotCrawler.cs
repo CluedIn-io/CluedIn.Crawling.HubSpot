df863cdafbbeed7e500cc48607fb815007567df4
Ben Fidge <ben.fidge@btopenworld.com>
2019-06-13 11:01:11 +0700

    Added Get Companies chain of calls

--- src/Hubspot.Crawling/HubspotCrawler.cs
+++ src/Hubspot.Crawling/HubspotCrawler.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using CluedIn.Core.Crawling;
 using CluedIn.Crawling.HubSpot.Core;
+using CluedIn.Crawling.HubSpot.Core.Models;
+using CluedIn.Crawling.HubSpot.Infrastructure;
 using CluedIn.Crawling.HubSpot.Infrastructure.Factories;
 
 namespace CluedIn.Crawling.HubSpot
@@ -28,19 +30,30 @@ namespace CluedIn.Crawling.HubSpot
             //TODO
             var settings = client.GetSettingsAsync().Result;
 
-            var companies = client.GetCompaniesAsync().Result;
-            foreach (var company in companies.results)
+            var companyProperties = client.GetCompanyPropertiesAsync(settings).Result;
+
+            foreach (var o in GetCompaniesAndAssociatedObjects(client, companyProperties, settings))
             {
-                yield return company;
-                if (company.companyId.HasValue)
-                {
-                    var contacts = client.GetContactsByCompanyAsync(company.companyId.Value).Result;
-                    foreach(var contact in contacts.contacts)
-                    {
-                        yield return contact;
-                    }
-                }
+                yield return o;
             }
+
+
+            var dealProperties = client.GetDealPropertiesAsync(settings).Result;
+
+
+            //var companies = client.GetCompaniesAsync().Result;
+            //foreach (var company in companies.results)
+            //{
+            //    yield return company;
+            //    if (company.companyId.HasValue)
+            //    {
+            //        var contacts = client.GetContactsByCompanyAsync(company.companyId.Value).Result;
+            //        foreach(var contact in contacts.contacts)
+            //        {
+            //            yield return contact;
+            //        }
+            //    }
+            //}
 
             //foreach( var folder in client.GetFolders())
             //{
@@ -51,6 +64,65 @@ namespace CluedIn.Crawling.HubSpot
             //    }
             //}
             throw new NotImplementedException();
+        }
+
+        private static IEnumerable<object> GetCompaniesAndAssociatedObjects(HubSpotClient client, List<string> companyProperties, Settings settings)
+        {
+            int offset = 0;
+            long portalId = 0;
+            while (true)
+            {
+                var companies = client.GetCompaniesAsync(companyProperties, 100, offset).Result;
+
+                if (companies.results != null)
+                {
+                    foreach (var company in companies.results)
+                    {
+                        if (settings != null && settings.currency != null)
+                            company.Currency = settings.currency;
+
+                        if (company.portalId.HasValue)
+                            portalId = company.portalId.Value;
+
+                        yield return company;
+
+                        if (company.companyId.HasValue)
+                        {
+                            var contacts = client.GetContactsByCompanyAsync(company.companyId.Value).Result;
+                            foreach (var contact in contacts.contacts)
+                            {
+                                yield return contact;
+                            }
+
+                            var engagements = client.GetEngagementByIdAndTypeAsync(company.companyId.Value, "COMPANY").Result;
+                            foreach (var engagement in engagements)
+                            {
+                                yield return engagement;
+                            }
+                        }
+                    }
+                }
+
+                if (companies.results == null || companies.hasMore == false)
+                    break;
+
+                if (companies.offset == null)
+                    break;
+
+                if (companies.results.Count < 100)
+                    break;
+
+                offset = companies.offset.Value;
+
+                //System.Threading.Tasks.Task.Delay(TimeSpan.FromSeconds(10)).Wait(state.CancellationTokenSource.Token);
+            }
+
+            // TODO Is this correct? Just get deal pipelines for last company portal id?
+            var dealPipelines = client.GetDealPipelinesAsync(portalId);
+            foreach (var dealPipeline in dealPipelines.Result)
+            {
+                yield return dealPipeline;
+            }
         }
     }
 }
--- src/Hubspot.Infrastructure/HubspotClient.cs
+++ src/Hubspot.Infrastructure/HubspotClient.cs
@@ -4,9 +4,11 @@ using CluedIn.Core.Providers;
 using CluedIn.Crawling.HubSpot.Core;
 using CluedIn.Crawling.HubSpot.Core.Models;
 using System.Collections.Generic;
+using System.Linq;
 using RestSharp;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Task = CluedIn.Crawling.HubSpot.Core.Models.Task;
 
 namespace CluedIn.Crawling.HubSpot.Infrastructure
 {
@@ -31,23 +33,119 @@ namespace CluedIn.Crawling.HubSpot.Infrastructure
             _client.AddDefaultParameter("hapikey", hubspotCrawlJobData.ApiToken, ParameterType.QueryString);
         }
 
-        public async Task<List<PropertyDefinition>> GetCompanyPropertiesAsync() =>
-            await GetAsync<List<PropertyDefinition>>("properties/v1/companies/properties");
+        public async Task<Settings> GetSettingsAsync() => await GetAsync<Settings>("integrations/v1/me");
+
+        public async Task<List<string>> GetCompanyPropertiesAsync(Settings settings)
+        {
+            var items = await GetAsync<List<PropertyDefinition>>("properties/v1/companies/properties");
+
+            var properties = items.Select(s => s.Name).ToList();
+            if (!properties.Contains("name"))
+                properties.Add("name");
+            if (!properties.Contains("companyname"))
+                properties.Add("companyname");
+            if (!properties.Contains("about_us"))
+                properties.Add("about_us");
+            if (!properties.Contains("numemployees"))
+                properties.Add("numemployees");
+            if (!properties.Contains("annual"))
+                properties.Add("annual");
+            if (!properties.Contains("company_notes"))
+                properties.Add("company_notes");
+            if (!properties.Contains("first_deal_created_date"))
+                properties.Add("first_deal_created_date");
+            if (!properties.Contains("industry"))
+                properties.Add("industry");
+            if (!properties.Contains("hs_analytics_source"))
+                properties.Add("hs_analytics_source");
+            if (!properties.Contains("hs_lastmodifieddate"))
+                properties.Add("hs_lastmodifieddate");
+            if (!properties.Contains("integrations"))
+                properties.Add("integrations");
+            if (!properties.Contains("domain"))
+                properties.Add("domain");
+            if (!properties.Contains("facebook_company_page"))
+                properties.Add("facebook_company_page");
+            if (!properties.Contains("founded_year"))
+                properties.Add("founded_year");
+            if (!properties.Contains("is_public"))
+                properties.Add("is_public");
+            if (!properties.Contains("linkedin_company_page"))
+                properties.Add("linkedin_company_page");
+            if (!properties.Contains("num_associated_contacts"))
+                properties.Add("num_associated_contacts");
+            if (!properties.Contains("timezone"))
+                properties.Add("timezone");
+            if (!properties.Contains("days_to_close"))
+                properties.Add("days_to_close");
+            if (!properties.Contains("description"))
+                properties.Add("description");
+
+            return properties;
+        }
+
+        public async Task<IEnumerable<object>> GetEngagementByIdAndTypeAsync(long objectId, string objectType)
+        {
+            var result = new List<object>();
+            int? offset = 0;
+            while (true)
+            {
+                var queryStrings = new List<QueryStringParameter>
+                {
+                    new QueryStringParameter("limit", "20"),
+                    new QueryStringParameter("offset", offset)
+                };
+
+                var response = await GetAsync<EngagementResponse>($"/engagements/v1/engagements/associated/{objectType}/{objectId}/paged",
+                    queryStrings);
+
+                if (response?.results == null)
+                    return result;
 
 
-        public async Task<Settings> GetSettingsAsync() =>
-            await GetAsync<Settings>("integrations/v1/me");
+                foreach (var engagement in response.results)
+                {
+                    if (engagement?.engagement == null)
+                        continue;
 
-        public async Task<CompanyResponse> GetCompaniesAsync(int limit = 100, int offset = 0) =>
-           await GetAsync<CompanyResponse>("companies/v2/companies/paged",
-               new QueryStringParameter("limit", limit ),
-               new QueryStringParameter("offset", offset)
-               );
+                    foreach (var c in engagement.Converttospecific())
+                        result.Add(c);
+                }
+
+                if (!response.results.Any())
+                    break;
+
+                if (!response.hasMore)
+                    break;
+
+                offset = response.offset;
+
+
+            }
+
+            return result;
+        }
+
+
+        public async Task<PropertyDefinition> GetDealPropertiesAsync(Settings settings) =>
+            await GetAsync<PropertyDefinition>("properties/v1/deals/properties");
+
+
+
+        public async Task<CompanyResponse> GetCompaniesAsync(IList<string> properties, int limit = 100, int offset = 0)
+        {
+            var queryStrings = properties.Select(n => new QueryStringParameter("properties", n)).ToList();
+            queryStrings.Insert(0, new QueryStringParameter("offset", offset));
+            queryStrings.Insert(0, new QueryStringParameter("limit", limit));
+
+            return await GetAsync<CompanyResponse>("companies/v2/companies/paged", queryStrings);
+        }
+
 
         public async Task<DealResponse> GetRecentDealsAsync(DateTime since, int count = 100, int offset = 0) =>
             throw new NotImplementedException();
 
-        public async Task<DealResponse> GetDealsAsync(int limit = 100, int offset = 0 /*TODO properties*/) =>
+        public async Task<DealResponse> GetDealsAsync(IList<string> properties, Settings settings, int limit = 100, int offset = 0) =>
           throw new NotImplementedException();
 
         public async Task<EngagementResponse> GetEngagementsAsync(int limit = 100, int offset = 0 /*TODO properties*/) =>
@@ -58,13 +156,13 @@ namespace CluedIn.Crawling.HubSpot.Infrastructure
         public async Task<CompanyResponse> GetRecentlyModifiedCompaniesAsync(int count = 100, int offset = 0) =>
                 throw new NotImplementedException();
 
-        public async Task<List<DealPipeline>> GetDealPipelinesAsync() =>
+        public async Task<List<DealPipeline>> GetDealPipelinesAsync(long portalId) =>
                 throw new NotImplementedException();
 
         public async Task<ContactResponse> GetContactsByCompanyAsync(long companyId) =>
             await GetAsync<ContactResponse>($"companies/v2/companies/{companyId}/contacts");
 
-        private async Task<T> GetAsync<T>(string url, params QueryStringParameter[] parameters)
+        private async Task<T> GetAsync<T>(string url, IList<QueryStringParameter> parameters = null)
         {
             var request = new RestRequest(url, Method.GET);
             if (parameters != null)
