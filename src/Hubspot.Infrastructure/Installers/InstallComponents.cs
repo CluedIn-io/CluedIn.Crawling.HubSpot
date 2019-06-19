2382521cccee5dba840460d88e04f1aeb4e04709
kevinobee1 <kvn@cluedin.com>
2019-06-19 04:51:58 +0100

    Add braces for clarity

--- src/Hubspot.Infrastructure/Installers/InstallComponents.cs
+++ src/Hubspot.Infrastructure/Installers/InstallComponents.cs
@@ -9,18 +9,20 @@ using RestSharp;
 
 namespace CluedIn.Crawling.HubSpot.Infrastructure.Installers
 {
-  public class InstallComponents : IWindsorInstaller
-  {
-    public void Install(IWindsorContainer container, IConfigurationStore store)
+    public class InstallComponents : IWindsorInstaller
     {
-      container
-          .AddFacilityIfNotExists<TypedFactoryFacility>()
-          .Register(Component.For<IHubSpotClientFactory>().AsFactory())
-          .Register(Component.For<IHubSpotImageFetcher, HubSpotImageFetcher>())
-          .Register(Component.For<HubSpotClient>().LifestyleTransient());
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container
+                .AddFacilityIfNotExists<TypedFactoryFacility>()
+                .Register(Component.For<IHubSpotClientFactory>().AsFactory())
+                .Register(Component.For<IHubSpotImageFetcher, HubSpotImageFetcher>())
+                .Register(Component.For<HubSpotClient>().LifestyleTransient());
 
-      if (!container.Kernel.HasComponent(typeof(IRestClient)) && !container.Kernel.HasComponent(typeof(RestClient)))
-        container.Register(Component.For<IRestClient, RestClient>());
+            if (!container.Kernel.HasComponent(typeof(IRestClient)) && !container.Kernel.HasComponent(typeof(RestClient)))
+            {
+                container.Register(Component.For<IRestClient, RestClient>());
+            }
+        }
     }
-  }
 }
