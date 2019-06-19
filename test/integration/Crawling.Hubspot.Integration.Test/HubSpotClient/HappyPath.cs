e6d9db8594d06df9c34a856837d23f0bdce5868d
kevinobee1 <kvn@cluedin.com>
2019-06-19 03:47:54 +0100

    [WIP] define HubSpot client integration tests

--- test/integration/Crawling.Hubspot.Integration.Test/HubSpotClient/HappyPath.cs
+++ test/integration/Crawling.Hubspot.Integration.Test/HubSpotClient/HappyPath.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using AutoFixture.Xunit2;
 using CluedIn.Core.Logging;
 using CluedIn.Crawling.HubSpot.Core;
 using CluedIn.Crawling.HubSpot.Core.Models;
 using Moq;
 using RestSharp;
 using Xunit;
-using Client = CluedIn.Crawling.HubSpot.Infrastructure.HubSpotClient;
 using Task = System.Threading.Tasks.Task;
 
 namespace Crawling.HubSpot.Integration.Test.HubSpotClient
 {
     public class HappyPath
     {
-        private readonly Client _sut;
+        private readonly CluedIn.Crawling.HubSpot.Infrastructure.HubSpotClient _sut;
 
         public HappyPath()
         {
@@ -20,7 +21,8 @@ namespace Crawling.HubSpot.Integration.Test.HubSpotClient
 
             var logger = new Mock<ILogger>();
 
-            _sut = new Client(logger.Object, crawlJobData, new RestClient());
+            _sut = new CluedIn.Crawling.HubSpot.Infrastructure.HubSpotClient(logger.Object, crawlJobData,
+                new RestClient());
         }
 
         [Fact]
@@ -32,22 +34,126 @@ namespace Crawling.HubSpot.Integration.Test.HubSpotClient
         [Fact]
         public async Task SettingsAreAvailable()
         {
-            var settings = await _sut.GetSettingsAsync();
-            Assert.NotNull(settings);
+            Assert.NotNull(
+                await _sut.GetSettingsAsync());
+        }
+
+        [Theory]
+        [InlineAutoData]
+        public async Task CompanyPropertiesAreAvailable(Settings settings)
+        {
+            Assert.NotEmpty(
+                await _sut.GetCompanyPropertiesAsync(settings));
+        }
+
+        [Theory]
+        [InlineAutoData]
+        public async Task UpTo100CompaniesAreAvailable(List<string> properties)
+        {
+            Assert.InRange(
+                (await _sut.GetCompaniesAsync(properties)).results.Count,
+                0,100);
+        }
+
+        [Theory]
+        [InlineAutoData]
+        public async Task ContactPropertiesAreAvailable(Settings settings)
+        {
+            Assert.NotEmpty(
+                await _sut.GetContactPropertiesAsync(settings));
+        }
+
+        [Theory]
+        [InlineAutoData]
+        public async Task UpTo20ResultsInEngagementsQueryableByIdAndType(long objectId, string objectType)
+        {
+            Assert.InRange(
+                (await _sut.GetEngagementByIdAndTypeAsync(objectId, objectType)).Count(),
+                0, 20);
+        }
+
+        [Theory]
+        [InlineAutoData]
+        public async Task DealPropertiesAreAvailable(Settings settings)
+        {
+            Assert.NotEmpty(
+                await _sut.GetDealPropertiesAsync(settings));
+        }
+
+        [Theory]
+        [InlineAutoData]
+        public async Task ProductPropertiesAreAvailable(Settings settings)
+        {
+            Assert.NotEmpty(
+                await _sut.GetProductPropertiesAsync(settings));
+        }
+
+        [Theory]
+        [InlineAutoData]
+        public async Task UpTo100ProductsAreAvailable(List<string> properties)
+        {
+            Assert.InRange(
+                (await _sut.GetProductsAsync(properties)).Objects.Count,
+                0, 100);
+        }
+
+        [Theory]
+        [InlineAutoData]
+        public async Task LineItemPropertiesAreAvailable(Settings settings)
+        {
+            Assert.NotEmpty(
+                await _sut.GetLineItemPropertiesAsync(settings));
+        }
+
+        [Theory]
+        [InlineAutoData]
+        public async Task UpTo100LineItemsAreAvailable(List<string> properties)
+        {
+            Assert.InRange(
+                (await _sut.GetLineItemsAsync(properties)).Objects.Count,
+                0, 100);
+        }
+
+        [Theory]
+        [InlineAutoData]
+        public async Task TicketPropertiesAreAvailable(Settings settings)
+        {
+            Assert.NotEmpty(
+                await _sut.GetTicketPropertiesAsync(settings));
+        }
+
+        [Theory]
+        [InlineAutoData]
+        public async Task UpTo100TicketsAreAvailable(List<string> properties)
+        {
+            Assert.InRange(
+                (await _sut.GetTicketsAsync(properties)).Objects.Count,
+                0, 100);
         }
 
         [Fact]
-        public async Task CompanyPropertiesAreAvailable()
+        public async Task UpTo20DynamicContactListsAreAvailable()
         {
-            var companyProperties = await _sut.GetCompanyPropertiesAsync(new Settings());
-            Assert.NotNull(companyProperties);
+            Assert.InRange(
+                (await _sut.GetDynamicContactListsAsync()).contacts.Count,
+                0, 20);
         }
 
         [Fact]
-        public async Task UpTo100CompaniesAreAvailable()
+        public async Task UpTo20StaticContactListsAreAvailable()
         {
-            var companies = await _sut.GetCompaniesAsync(new List<string>());
-            Assert.InRange(companies.results.Count,1,100);
+            Assert.InRange(
+                (await _sut.GetStaticContactListsAsync()).contacts.Count,
+                0, 20);
+        }
+
+        [Theory]
+        [InlineAutoData]
+        public async Task UpTo100DealAssociationsAreAvailable(int objectId)
+        {
+            Assert.InRange(
+                (await _sut.GetDealAssociationsAsync(objectId)).Results.Count,
+                0, 100);
         }
     }
 }
