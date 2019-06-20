85ef6013ea9d9f85211dab7d1ea505fb895eaa6b
kevinobee1 <kvn@cluedin.com>
2019-06-20 05:18:32 +0100

    Integration tests point to wiremock docker instance

--- test/integration/Crawling.Hubspot.Integration.Test/HubspotConfiguration.cs
+++ test/integration/Crawling.Hubspot.Integration.Test/HubspotConfiguration.cs
@@ -8,7 +8,7 @@ namespace Crawling.HubSpot.Integration.Test
         public static Dictionary<string, object> Create()
         {
             return new Dictionary<string, object> {
-                { HubSpotConstants.KeyName.BaseUri, "https://api.hubapi.com/" },
+                { HubSpotConstants.KeyName.BaseUri, "http://127.0.0.1:8080/" },
                 { HubSpotConstants.KeyName.ApiToken,"4fad15b1-8d51-4919-b11a-125bd9346e51" },
                 { HubSpotConstants.KeyName.CustomerSubDomain, "" }
             };
