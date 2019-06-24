7e71673decff32acddce43a63242d12f903f8deb
Ben Fidge <ben.fidge@btopenworld.com>
2019-06-24 11:31:37 +0700

    Added unit tests for IHubSpotImageTester

---
+++ test/unit/Provider.Hubspot.Unit.Test/ResourceHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Provider.HubSpot.Unit.Test
+{
+    public static class ResourceHelper
+    {
+        public static Stream GetFile(string name)
+        {
+            Assembly a = Assembly.GetExecutingAssembly();
+
+            var @namespace = a.FullName.Split(',')[0];
+            Stream s = a.GetManifestResourceStream(@namespace + "." + name);
+
+            return s;
+        }
+
+        public static IEnumerable<string> GetFilenames(string partialPath)
+        {
+            Assembly a = Assembly.GetExecutingAssembly();
+            var @namespace = a.FullName.Split(',')[0] + ".";
+            var files = a.GetManifestResourceNames().Where(n => n.StartsWith(partialPath))
+                .Select(n => n.Replace(@namespace, "")).AsEnumerable();
+
+            return files;
+        }
+    }
+}
