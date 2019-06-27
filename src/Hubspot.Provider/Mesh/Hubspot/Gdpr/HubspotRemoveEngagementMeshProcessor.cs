c9e4ad39d6c332b62d672cda43f38b972180453e
Ben Fidge <ben.fidge@btopenworld.com>
2019-06-27 15:33:02 +0700

    Ported Mesh/Gdpr classes from main Repo. NOTE: NOT TESTED or tidied up to use HubSpot client

---
+++ src/Hubspot.Provider/Mesh/Hubspot/Gdpr/HubspotRemoveEngagementMeshProcessor.cs
@@ -0,0 +1,15 @@
+﻿using CluedIn.Core;
+
+namespace CluedIn.Provider.HubSpot.Mesh.Hubspot.Gdpr
+{
+    public class HubspotRemoveEngagementMeshProcessor : HubspotRemoveBaseMeshProcessor
+    {
+        public HubspotRemoveEngagementMeshProcessor(ApplicationContext appContext)
+            : base(appContext,  "engagements/v1/engagements/", CluedIn.Core.Data.EntityType.Task, CluedIn.Core.Data.EntityType.Activity)
+        {
+        }
+    }
+}
+
+
+
---
+++ src/Hubspot.Provider/Mesh/Hubspot/HubspotContactMeshProcessor.cs
@@ -0,0 +1,13 @@
+﻿using CluedIn.Core;
+
+namespace CluedIn.Provider.HubSpot.Mesh.Hubspot
+{
+    public class HubspotContactMeshProcessor : HubspotUpdateBaseMeshProcessor
+    {
+        public HubspotContactMeshProcessor(ApplicationContext appContext)
+            : base(appContext, "contacts/v1/contact/vid/:vid/profile", CluedIn.Core.Data.EntityType.Infrastructure.Contact, CluedIn.Core.Data.EntityType.Person)
+        {
+        }
+
+    }
+}
