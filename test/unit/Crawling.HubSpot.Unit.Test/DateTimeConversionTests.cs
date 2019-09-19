4512b5ac7779b6d114a9422d78024f8327b1b828
Ben Fidge <ben.fidge@btopenworld.com>
2019-09-19 13:33:03 +0700

    Changed all clue properties with dates to Is 8601

---
+++ test/unit/Crawling.HubSpot.Unit.Test/DateTimeConversionTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using CluedIn.Core.Utilities;
+using Xunit;
+
+namespace Crawling.HubSpot.Unit.Test
+{
+    public class DateTimeConversionTests
+    {
+        [Theory, InlineData(1560992400000)]
+        public void CheckCanConvertFromUnixEpochToDateTime(long epoch)
+        {
+            var date = DateUtilities.EpochRef.AddMilliseconds(epoch);
+            var iso8601 = date.ToString("O");
+
+            DateTimeOffset dummy;
+            Assert.True(DateTimeOffset.TryParse(iso8601, out dummy));
+            var result = DateTimeOffset.Parse(iso8601, CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal);
+
+
+
+        }
+    }
+}
